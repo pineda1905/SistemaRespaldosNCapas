23b76fbb6241fd76b2e2ffe1e7da32d267807b3a
Alex <lalafaro6@gmail.com>
2026-04-28 19:00:41 -0600

    clases actualizadas

---
+++ SistemRespaldo/SistemaRespaldo.DAL/ConfiguracionHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace SistemaRespaldo.DAL
+{
+    public static class ConfiguracionHelper
+    {
+        public static string CadenaConexion { get; private set; }
+        public static string RutaMysqlDump { get; private set; }
+        public static string RutaGuardado { get; private set; }
+
+        static ConfiguracionHelper()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("config.json", optional: false, reloadOnChange: true);
+
+            IConfiguration config = builder.Build();
+
+            string servidor = config["ConfiguracionServidor:Servidor"];
+            string puerto = config["ConfiguracionServidor:Puerto"];
+            string usuario = config["ConfiguracionServidor:Usuario"];
+            string password = config["ConfiguracionServidor:Password"];
+            string bd = config["ConfiguracionServidor:BaseDatosConfig"];
+
+            CadenaConexion = $"Server={servidor};Port={puerto};Database={bd};Uid={usuario};Pwd={password};";
+            RutaMysqlDump = config["Rutas:RutaMysqlDump"];
+            RutaGuardado = config["Rutas:RutaGuardadoRespaldos"];
+        }
+    }
+}
